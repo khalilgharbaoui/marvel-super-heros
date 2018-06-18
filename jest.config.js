f873517357a6af6e52ee5539dca5a43671ceed28
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-06-18 03:20:19 +0200

    Add jest en jest configurations

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+const {defaults} = require('jest-config');
+module.exports = {
+  setupFiles: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "identity-obj-proxy",
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy"
+  }
+}
