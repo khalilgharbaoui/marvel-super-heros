394eb19eda168433d8b14a470bb9c11466a1c660
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-06-18 03:28:21 +0200

    Use react-router-dom BrowserRouter as Router no need for history

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router, Route, IndexRoute, Link} from 'react-router-dom'
-import createBrowserHistory from 'history/createBrowserHistory'
+import {BrowserRouter as Router, Route, IndexRoute, Link} from 'react-router-dom'
 import Pace from 'pace-js';
 import './stylesheets/pace-theme-loading-bar.scss'
 import './stylesheets/animate.scss'
-import App from './components/App';
+import App from './components/App/App';
 import HerosList from './components/HerosList/HerosList';
 // import ComicsList from  './components/ComicsList';
 import Hero from './components/Hero/Hero';
-const browserHistory = createBrowserHistory();
 
-ReactDOM.render((<Router history={browserHistory}>
+ReactDOM.render((<Router>
   <App>
     <Route exact={true} path="/" component={HerosList}/>
     <Route path='/heros/:heroId' component={Hero}/>
